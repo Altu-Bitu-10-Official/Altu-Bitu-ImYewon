4a5b23cdf582bc01faf23cfe0f250311ef7bdd2d
yeone <yeone29@ewha.ac.kr>
2026-05-11 21:46:33 +0900

    [동적계획법] 5월 11일

---
+++ 07_동적계획법/필수/11053.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> a(n); 
+	vector<int> dp(n, 0);
+	int len = 0;
+	int answer = 0;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+		len = 0;
+
+		for (int j = 0; j < i; j++) {
+			if (a[j] < a[i]) {
+				len = max(len, dp[j]);
+			}
+		}
+		dp[i] = len + 1;
+		answer = max(answer, dp[i]);
+		
+	}
+
+	cout << answer;
+
+	return 0;
+}
