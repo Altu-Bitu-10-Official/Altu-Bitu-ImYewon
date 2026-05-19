853c38ae35ee3bf9b63a580bb6f6bbab92cece83
yeone <yeone29@ewha.ac.kr>
2026-05-19 15:26:18 +0900

    [DFS&BFS] 5월 19일

---
+++ 08_DFS&BFS/필수/2606.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n, m; // n: 컴퓨터 수, m: 연결된 컴퓨터 쌍
+int graph[101][101] = {0, };
+bool visited[101] = {false, };
+int cnt = 0; // 감염된 컴퓨터 수
+
+void bfs() {
+	queue<int> q;
+
+	q.push(1); // 1번 컴퓨터가 바이러그에 걸림
+	visited[1] = true;
+
+	while (!q.empty()) {
+		int now = q.front();
+		q.pop();
+
+		for (int i = 1; i <= n; i++) {
+			if (graph[now][i] == 1 && visited[i] == false) {
+				visited[i] = true;
+				q.push(i);
+				cnt++;
+			}
+		}
+	}
+
+}
+
+int main() {
+
+	cin >> n >> m;
+	
+	for (int i = 0; i < m; i++) {
+		int tmp1, tmp2;
+		cin >> tmp1 >> tmp2;
+		graph[tmp1][tmp2] = graph[tmp2][tmp1] = 1;
+	}
+
+	bfs();
+
+	cout << cnt;
+
+	return 0;
+}
---
+++ 08_DFS&BFS/필수/2644.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n, m; // n: 전체 사람 수, m: 관계의 개수
+bool visited[101] = {false, };
+int graph[101][101] = {0, };
+int n1, n2; // 촌수를 계산해야하는 두사람
+int dist[101]; // 촌수를 저장하는 배열
+
+void bfs() {
+	queue<int> q;
+
+	q.push(n1);
+	visited[n1] = true;
+	dist[n1] = 0; // 자기 자신과는 0촌
+
+	while (!q.empty()) {
+		int now = q.front();
+		q.pop();
+
+		for (int i = 1; i <= n; i++) {
+			if (graph[now][i] == 1 && visited[i] == false) {
+				visited[i] = true;
+				dist[i] = dist[now] + 1; // 촌수 + 1
+				q.push(i);
+			}
+		}
+	}
+
+}
+
+int main() {
+	cin >> n;
+	cin >> n1 >> n2;
+	cin >> m;
+
+	for (int i = 0; i < m; i++) {
+		int parent, child;
+		cin >> parent >> child;
+
+		graph[parent][child] = graph[child][parent] = 1;
+	}
+
+	bfs();
+
+	if (visited[n2] == true) {
+		cout << dist[n2];
+	} 
+	else {
+		cout << -1;
+	}
+
+	return 0;
+}
